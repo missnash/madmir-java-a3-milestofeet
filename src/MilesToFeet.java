6a1f45328bd795c243ca277d24dc92a3a2cea7db
clydewbrown <clydewbrown@gmail.com>
2013-08-12 07:34:56 -0400

    Files for Assignment #3 - Miles to Feet Conversion

---
+++ src/MilesToFeet.java
@@ -0,0 +1,12 @@
+
+public class MilesToFeet {
+
+	public static void main(String[] args) {
+		// -------INSTRUCTIONS-------
+		// 1. Refer to exercise 4 on page 113 of the Joyce Farrell book (chapter 2 PDF)
+		// 2. In this file, complete part (a) of this exercise
+		// 3. For this class, you WILL NOT receive an input from the user.
+
+	}
+
+}
---
+++ src/MilesToFeetInteractive.java
@@ -0,0 +1,12 @@
+
+public class MilesToFeetInteractive {
+
+	public static void main(String[] args) {
+		// -------INSTRUCTIONS-------
+		// 1. Refer to exercise 4 on page 113 of the Joyce Farrell book (chapter 2 PDF)
+		// 2. In this file, complete part (b) of this exercise
+		// 3. For this class, you WILL receive an input from the user.
+
+	}
+
+}
